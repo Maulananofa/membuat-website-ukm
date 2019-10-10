bffff00596b88b6a96022f4a7d9ea29a434e77fd
nofa34 <novaaku145@gmail.com>
2019-10-10 19:31:20 +0700

    membuat website ukm

---
+++ application/views/welcome_message.php
@@ -0,0 +1,248 @@
+  <!DOCTYPE html>
+  <html lang="">
+  	<head>
+  		<meta charset="utf-8">
+  		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<title>websiteukm.com</title>
+  
+  		<!-- Bootstrap CSS -->
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  		<!-- Google Fonts -->
+  		<link href='https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700' rel='stylesheet' type='text/css'>
+  		<!-- Font Awesome -->
+  		<link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
+  		<!-- Style -->
+  		<link href='style.css' rel='stylesheet' type='text/css'>
+  
+  		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  		<!--[if lt IE 9]>
+  			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  		<![endif]-->
+  	</head>
+  	<body>
+  		<nav class="navbar navbar-default" role="navigation">
+  			<div class="container">
+  				<!-- Brand and toggle get grouped for better mobile display -->
+  				<div class="navbar-header">
+  					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+  						<span class="sr-only">Toggle navigation</span>
+  						<span class="icon-bar"></span>
+  						<span class="icon-bar"></span>
+  						<span class="icon-bar"></span>
+  					</button>
+  					<a class="navbar-brand" href="#">Website UKM</a>
+  				</div>
+  		
+  				<!-- Collect the nav links, forms, and other content for toggling -->
+  				<div class="collapse navbar-collapse navbar-ex1-collapse">
+  					<ul class="nav navbar-nav navbar-right">
+  						<li><a href="#">Home</a></li>
+  						<li><a href="#">About </a></li>
+  						<li><a href="#">Blog</a></li>
+  						<li><a href="#">services</a></li>
+  						<li><a href="#">Contact</a></li>
+  					</ul>
+  				</div><!-- /.navbar-collapse -->
+  			</div>
+  		</nav>
+		
+		<!-- START SECTION -->
+  		<div class="section hero text-center background-dark dark-bg">
+  			<div class="background-image" style="background: url(images/dd.jpg) no-repeat center center; background-size: cover; "></div>
+  			<div class="container">
+  				<div class="row">
+  					<div class="col-md-12">
+  						<h2 class="text-info">UNEVERSITAS GADJAH MADA</h2>
+  						<h2 class="text-primary">Profil Unit Kegiatan Mahasiswa</h2>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  		<!--/.section -->
+
+  		<!-- START SECTION -->
+  		<div class="section background-light">
+  			<div class="container">
+  				<div class="row">
+  					<div class="col-md-12 text-center">
+  						<h2 class="section-title">Unit kegiatan mahasiswa</h2>
+  						<p class="section-description">wadah aktivitas kemahasiswaan luar kelas untuk mengembangkan minat, bakat dan keahlian tertentu. Lembaga ini merupakan partner organisasi kemahasiswaan intra kampus lainnya seperti senat mahasiswa dan badan eksekutif mahasiswa, baik yang berada di tingkat program studi, jurusan, maupun universitas. Lembaga ini bersifat otonom, dan bukan sebagai cabang dari badan eksekutif maupun senat mahasiswa.</p>
+  					</div>
+  					<div class="col-md-4">
+  						<div class="service-block text-center">
+  							<h2>Sekber Kerohanian</h2>
+  							<p>1. Keluarga Mahasiswa Buddhis <br> 2.Unit Kerohanian Kristen <br> 3.Keluarga Mahasiswa Hindu Dharma <br>4. Misa Kampus</p>
+  							<a href="#" title="Learn More" class="btn btn-sm btn-primary">Learn More</a>
+  						</div><!--/.service-block-->
+  					</div>
+  					<div class="col-md-4">
+  						<div class="service-block text-center">
+  							<h2>Sekber kesenian</h2>
+  							<p> *Teater Gadjah Mada <br>* Paduan Suara Mahasiswa (PSM) UGM<br>* Gama Band
+                  <br>* Unit Fotografi<br>* Unit Seni Rupa dll.</p>
+  							<a href="#" title="Learn More" class="btn btn-sm btn-primary">Learn More</a>
+  						</div><!--/.service-block-->
+  					</div>
+  					<div class="col-md-4">
+  						<div class="service-block text-center">
+  							<h2>Sekber khusus</h2>
+  							<p>#Satuan Resimen Mahasiswa (Satmenwa) UGM <br># Gama Cendekia<br>#EDS (English Debating Society) <br>#UPI Interdisipliner <br># Pramuka dll.</p>
+  							<a href="#" title="Learn More" class="btn btn-sm btn-primary">Learn More</a>
+  						</div><!--/.service-block-->
+  					</div>
+            <div class="col-md-4">
+              <div class="service-block text-center">
+                <h2>Sekber Olahraga</h2>
+                <p>1.basket <br>2. volleyball <br>3.bulutangkis <br>4.futsal <br>5. hockey dll.</p>
+                <a href="#" title="Learn More" class="btn btn-sm btn-primary">Learn More</a>
+              </div><!--/.service-block-->
+            </div>
+  				</div>
+  			</div>
+  		</div>
+  		<!--/.section -->
+
+  		<!-- START SECTION -->
+  		<div class="section hero text-center background-primary dark-bg">
+  			<div class="background-image" style="background: url(images/OO.jpg) no-repeat fixed center center; background-size: cover;"></div>
+  			</div>
+  		</div>
+  		<!--/.section -->
+
+  		<!-- START SECTION -->
+  		<div class="section">
+  			<div class="container">
+  				<div class="row">
+  					<div class="col-md-12 text-center">
+  						<h1 class="section-title">GELANGGANG MAHASISWA</h1>
+  						<p class="section-description">Gelanggang Mahasiswa UGM adalah pusat kegiatan untuk para mahasiswa di Yogyakarta. Hanya saja karena letaknya berdekatan dengan Kampus UGM maka akhirnya identik dengan pusat kegiatan bagi mahasiswa UGM saja. Gelanggang Mahasiswa UGM dibangun tahun 1970-an dan sempat menjadi sentra pergerakan bagi para aktivis tahun 1970-an ketika Dewan Mahasiswa UGM dan Dewan Mahasiswa se-Yogyakarta masih berkantor di gedung tersebut. Dari sejak berdiri hingga sekarang, Gelanggang Mahasiswa UGM telah menghasilkan belasan ribu aktivis kegiatan kemahasiswaan.</p>
+  					</div>
+  					<div class="col-md-4">
+  						<div class="entry">
+  							<div class="entry-media">
+  								<a href="#" title="">
+  									<span class="absolute-wrapper"></span>
+  									<img src="images/ll.jpg" alt="" class="img-responsive" />
+  								</a>
+  							</div>
+  							<center><h1 class="entry-title"><a href="#" title="Yelp Is Awful for Everyone Involved">Volleyball</a></h1></center>
+  							<center><h4>*Jadwal latihan H kamis:19:00-21:00(pr)20:30-23:00 (lk) <br>* Hari Minggu:09-11 (pr)10.3:13:00 (lk) <br>* Tempat :Hall Gelangga Mahasiswa UGM <br> * kegiatan :latihan rutin <br>* DC :BERSEPATU DAN CELANA OLAHRAGA <br> * Pembibing:idham Muhammad H <br>* nomor TLP :085725354351 .</h4></center>
+  							<a href="#" title="Learn More" class="btn read-more btn-sm btn-primary">Learn More</h2>
+  						</div><!--/.entry-->
+  					</div>
+  					<div class="col-md-4">
+  						<div class="entry">
+  							<div class="entry-media">
+  								<a href="#" title="">
+  									<span class="absolute-wrapper"></span>
+  									<img src="images/pp.jpg" alt="" class="img-responsive" />
+  								</a>
+  							</div>
+  							<center><h1 class="entry-title"><a href="#" title="2 Best Sites to Build Your Own Website">BULUTANGKIS</a></h1></center>
+  							<center><H4>*persyaratan=KTM <br>* jadwal latihan=sabtu jam 11-04 <br>*jadwal untuk atlit =rabu dan sabtu 18:00-22:00 <br>*Nama Pembibing=ARI <br>*NO TLP=085778478499 .</H4></center>
+  							<a href="#" title="Learn More" class="btn read-more btn-sm btn-primary">Learn More</a>
+  						</div><!--/.entry-->
+  					</div>
+  					<div class="col-md-4">
+  						<div class="entry">
+  							<div class="entry-media">
+  								<a href="#" title="">
+  									<span class="absolute-wrapper"></span>
+  									<img src="images/bb.jpg" alt="" class="img-responsive" />
+  								</a>
+  							</div>
+  							<center><h1 class="entry-title"><a href="#" title="2 Best Sites to Build Your Own Website">SILAT BIMA</a></h1></center>
+  							<center><h4>*Persyaratan=Usia Minimal 14 Tahun <br>* jadwal latihan laki"=senin dan rabu 21:00-23:00 <br>* jadwal lahian pr= selasa dan kamis 20-21 <br>*nama pembibing=Catur Arianto <br>*NO TLP=082384834179.</h4></center>
+  							<a href="#" title="Learn More" class="btn read-more btn-sm btn-primary">Learn More</a>
+  						</div><!--/.entry-->
+  					</div>
+  				</div><!--/.row-->
+  			</div><!--/.container-->
+  		</div>
+  		<!--/.section -->
+
+  		<!-- START SECTION -->
+  		<div class="section hero text-center background-dark dark-bg">
+  			<div class="background-image" style="background: url(images/gg.jpg) no-repeat fixed center center; background-size: cover;"></div>
+  			<div class="container">
+  				<div class="row">
+  				</div>
+  			</div>
+  		</div>
+  		<!--/.section -->
+
+  		<!-- START SECTION -->
+  		<div class="section">
+  			<div class="container">
+  				<div class="row">
+  					<div class="col-md-12 text-center">
+  						<h1 class="section-title">DATA</h1>
+  						<h1 class="section-description"> Isi Data Di Bawah Ini!!.</h1>
+  					</div>
+  					<div class="col-md-8 col-md-offset-2">
+  						<form id="contact" method="post" class="form" role="form">
+							<div class="row">
+								<div class="col-md-6 form-group">
+									<input class="form-control" id="name" name="name" placeholder="Name" type="text" required />
+								</div>
+								<div class="col-md-6 form-group">
+									<input class="form-control" id="email" name="email" placeholder="Email" type="email" required />
+								</div>
+							</div>
+							<textarea class="form-control" id="message" name="message" placeholder="Message" rows="5"></textarea>
+							<br />
+							<div class="row">
+								<div class="col-md-12 form-group">
+									<button class="btn btn-primary" type="submit">Submit</button>
+								</div>
+							</div>
+						</form>
+  					</div>
+  				</div><!--/.row-->
+  			</div><!--/.container-->
+  		</div>
+  		<!--/.section -->
+
+  		<!-- START SECTION -->
+  		<div class="section hero text-center background-light">
+  			<div class="background-image" style="background: url(images/ww.jpg) no-repeat fixed center center; background-size: cover;"></div>
+  			<div class="container">
+  				<div class="row">
+  				</div>
+  			</div>
+  		</div>
+  		<!--/.section -->
+
+  		<!-- START SECTION -->
+  		<div class="section background-dark dark-bg">
+  			<div class="container">
+  				<div class="row">
+  					<div class="col-md-3">
+  						<h3 class="text-uppercase font-size-md letter-spacing-md font-weight-lg ">KERJA SAMA</h3>
+  						<p>*Kerja Sama Dalam Negeri <br>* Alumni <br>* Urusan Internasional</p>
+  					</div>
+  					<div class="col-md-4 col-md-offset-1">
+  						<h3 class="text-uppercase font-size-md letter-spacing-md font-weight-lg ">TENTANG UKM</h3>
+  					   <p>* Sambutan Rektor <br>* Sejarah <br> Visi dan Misi <br>* Pimpinan Universitas <br>* Manajemen</p>
+            </div>
+            <div class="col-md-4 col-md-offset-1">
+              <h3 class="text-uppercase font-size-md letter-spacing-md font-weight-lg ">ALAMAT UKM UGM</h3>
+               <p>Jl. Pancasila No.1, Bulaksumur, Caturtunggal, Sleman, Daerah Istimewa Yogyakarta 55281</P>
+            </div>
+  					<div class="col-md-12 margin-top-md text-center font-size-sm text-upercase">
+  						<p>© <a href="http://nomadtheme.com" title="nomadtheme"><strong>SMKN 1 BANGSRI</strong></a>.:<strong>2019</strong></a>.</p>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  		<!--/.section -->
+  
+  		<!-- jQuery -->
+  		<script src="//code.jquery.com/jquery.js"></script>
+  		<!-- Bootstrap JavaScript -->
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  	</body>
+  </html>
